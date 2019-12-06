f2fbee729477763a25b8d3076a7a8274a1a64f54
decovery <aiste@akmila.lt>
2019-12-06 19:12:54 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-      <div>
-        Event voter APP
-      </div>
-  );
+  return <div>Event voter APP / check if it's working</div>;
 }
 export default App;
