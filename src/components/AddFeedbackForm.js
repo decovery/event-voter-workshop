0fc6d43d93ba8a5fd9bd251f8406d226da221935
decovery <aiste@akmila.lt>
2019-12-07 15:29:52 +0200

    Step 10.01 - modal forms inputs added

--- src/components/AddFeedbackForm.js
+++ src/components/AddFeedbackForm.js
@@ -3,6 +3,11 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ThumbDownIcon from '@material-ui/icons/ThumbDown';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default function AddFeedbackForm({ onCancel, onSubmit }) {
   return (
@@ -15,9 +20,30 @@ export default function AddFeedbackForm({ onCancel, onSubmit }) {
     >
       <DialogTitle id="form-dialog-title">Add Feedback</DialogTitle>
       <DialogContent>
-        <div>*** Vertinimo forma ***</div>
+        <div>
+          <IconButton color={undefined} aria-label="Like">
+            <ThumbUpIcon />+{' '}
+          </IconButton>{' '}
+          <IconButton color={undefined} aria-label="Dislike">
+            <ThumbDownIcon />+{' '}
+          </IconButton>
+        </div>
+        <TextField label="Name" fullWidth margin="normal" />
+        <TextField
+          label="Your comments"
+          placeholder="Can add multiline text here"
+          multiline
+          fullWidth
+          margin="normal"
+          helperText="comments are optional"
+        />
       </DialogContent>
-      <DialogActions>*** Valdymo mygtukai ***</DialogActions>
+      <DialogActions>
+        <Button color="default">Cancel</Button>
+        <Button color="primary" variant="contained">
+          Submit
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
